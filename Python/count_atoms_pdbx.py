ab85adb950190a94cf2ad35d2098d0c0e6ccf9db
ChristinaLyu <clyu@smith.edu>
2017-05-18 12:15:10 -0400

    count lines

--- Python/count_atoms_pdbx.py
+++ Python/count_atoms_pdbx.py
@@ -1,14 +1,14 @@
 '''
-    File:           count_atoms_mmcif_lines.py
+    File:           count_atoms_pdbx.py
     Author:         Christina Lyu
     Date created:   5/15/17
     Updates:
-    Last modified:  5/15/17
+    Last modified:  5/18/17
     Python Version: 3.5.2
 
-    Description:    output the number of atoms in a mmCIF file
-    Run format:     python count_atoms_mmcif.py pathToInputCifFile pathToOutputTextFile
-    Run example:    python count_atoms_mmcif.py /Users/xxx/abc.cif /Users/xxx/
+    Description:    output the number of atoms in a pdbx file
+    Run format:     python count_atoms_pdbx.py pathToInputCifFile pathToOutputTextFile
+    Run example:    python count_atoms_pdbx.py /Users/xxx/abc.cif /Users/xxx/
 '''
 
 import os, sys
@@ -34,14 +34,16 @@ def main():
     outPath = sys.argv[2]
     file = open(filename, 'r')
     file = file.read()
-    index_1 = file.find('_atom_site.pdbx_PDB_model_num ')
-    index_2 = file.find('_pdbx_poly_seq_scheme.asym_id')
-    file = file[index_1 + 31 : index_2]
-    index_4 = file.find('#')
-    file = file[:index_4]
-    file = file.splitlines()
-    nrAtoms = len(file)
-    
+    nrAtoms = 0
+    while file.find('MODEL        ') != -1 and file.find('ENDMDL') != -1:        
+        index_1 = file.find('MODEL        ')
+        index_2 = file.find('ENDMDL')
+        cut = file[index_1: index_2 + 8]
+        cut = cut.splitlines()
+        cut = cut[1:-2]
+        nrAtoms = nrAtoms + len(cut)
+        file = file[index_2 + 8: ]
+           
     nrFile = open(outPath, 'w')
     nrFile.write(str(nrAtoms))
     nrFile.close()
--- Python/count_atoms_xml_lines.py
+++ Python/count_atoms_xml_lines.py
@@ -1,17 +1,17 @@
 '''
-    File:           count_atoms_mmcif_lines.py
+    File:           count_atoms_xml_lines.py
     Author:         Christina Lyu
     Date created:   5/15/17
     Updates:
-    Last modified:  5/15/17
+    Last modified:  5/18/17
     Python Version: 3.5.2
 
-    Description:    output the number of atoms in a mmCIF file
-    Run format:     python count_atoms_mmcif.py pathToInputCifFile pathToOutputTextFile
-    Run example:    python count_atoms_mmcif.py /Users/xxx/abc.cif /Users/xxx/
+    Description:    output the number of atoms in a xml file
+    Run format:     python count_atoms_xml.py pathToInputCifFile pathToOutputTextFile
+    Run example:    python count_atoms_xml.py /Users/xxx/abc.cif /Users/xxx/
 '''
-
 import os, sys
+
 DEBUG = True
 #-----------------------------------------------------------------      Utilities for debugging    ------
 def debugMark(message,functionName):
@@ -32,22 +32,16 @@ def debugVar(var,val):
 def main():
     filename = sys.argv[1]
     outPath = sys.argv[2]
+    listtttt = []
     file = open(filename, 'r')
     file = file.read()
-    nrAtoms = 0
-    while file.find('MODEL        ') != -1 and file.find('ENDMDL') != -1:        
-        index_1 = file.find('MODEL        ')
-        index_2 = file.find('ENDMDL')
-        cut = file[index_1: index_2 + 8]
-        cut = cut.splitlines()
-        cut = cut[1:-2]
-        nrAtoms = nrAtoms + len(cut)
-        file = file[index_2 + 8: ]
-           
-    nrFile = open(outPath, 'w')
-    nrFile.write(str(nrAtoms))
-    nrFile.close()
-main()
-    
-    
+    index_1 = file.find('<PDBx:atom_site id="')
+    index_2 = file.find('</PDBx:atom_siteCategory>')
+    file = file[index_1 : index_2]
+    lis = file.split('<PDBx:atom_site id="')
+    nrAtom = len(lis)
+    outFile = open(outPath, 'w')
+    outFile.write(str(nrAtom))
 
+main()
+                
